18302aac70b9a338a48f72dcea9bb746fbf75a0f
Daniel Martinez <dmartinez@itexico.net>
2017-02-13 12:49:45 -0600

    WEEK-2 Part 2: Implements getFleshScore

--- src/document/Document.java
+++ src/document/Document.java
@@ -154,7 +154,10 @@ public abstract class Document {
 	{
 	    // TODO: You will play with this method in week 1, and 
 		// then implement it in week 2
-	    return this.text.length();
+		int words = getNumWords();
+		int sentences = getNumSentences();
+		int syllables = getNumSyllables();
+	    return 206.835 - (1.015 * (words / sentences)) - (84.6 * (syllables / words));
 	}
 	
 	
